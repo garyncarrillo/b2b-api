6c51c2c4cde3d685f6fd279c571cd5eba85447dd
Harold Rangel <haroldrangelcharris@gmail.com>
2019-11-29 12:34:05 -0500

    Add missing attribute to jwt_blacklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_193457) do
+ActiveRecord::Schema.define(version: 2019_11_29_173247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "jwt_blacklist", force: :cascade do |t|
     t.string "jti", null: false
+    t.datetime "exp"
     t.index ["jti"], name: "index_jwt_blacklist_on_jti"
   end
 
