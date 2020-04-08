b3c0d580c1bfc8b8c72c759a3be744d323e83bf9
Harold Rangel <haroldrangelcharris@gmail.com>
2020-04-08 07:14:15 -0500

    Add ability to change password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,6 +16,6 @@ class UserController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :phone, :email, :company)
+    params.require(:user).permit(:first_name, :last_name, :phone, :email, :company, :password)
   end
 end
