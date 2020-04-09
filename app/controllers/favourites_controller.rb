c769e8ee2879ebf11717b6e475021a9b68d36420
Garyn Carrillo Caballero <garync@gmail.com>
2020-04-09 12:18:51 -0500

    Fix query pagination on favourites controller

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,6 +1,6 @@
 class FavouritesController < ApplicationController
   def index
-    favourites = Favourite.ransack({favouritable_type_cont: params[:q], user_id_eq: current_user.id})
+    favourites = current_user.favourites.ransack(params[:q])
     pagy, records = pagy(favourites.result, items: params[:items] || 5, page: params[:page])
     render json: { articles: FavouriteSerializer.new(records), metadata: generate_pagination_metadata(pagy) }, status: 200
   end
