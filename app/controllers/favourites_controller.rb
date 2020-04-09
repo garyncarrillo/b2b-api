0492025f9a0f9b99761740dc907857e550cd0e3f
Garyn Carrillo Caballero <garync@gmail.com>
2020-04-09 12:20:52 -0500

    Fix name collection return

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,6 +2,6 @@ class FavouritesController < ApplicationController
   def index
     favourites = current_user.favourites.ransack(params[:q])
     pagy, records = pagy(favourites.result, items: params[:items] || 5, page: params[:page])
-    render json: { articles: FavouriteSerializer.new(records), metadata: generate_pagination_metadata(pagy) }, status: 200
+    render json: { favourites: FavouriteSerializer.new(records), metadata: generate_pagination_metadata(pagy) }, status: 200
   end
 end
