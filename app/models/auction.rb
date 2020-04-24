230e2161d637d394cc097e058a36cc60fdcdf5bd
Harold Rangel <haroldrangelcharris@gmail.com>
2020-04-23 21:11:12 -0500

    Add enums for auctions and products

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -3,12 +3,14 @@ class Auction < ApplicationRecord
   validates :start_at, presence: true
   validates :description, presence: true
   validates :contact_phone, presence: true
-  validates :auction_type, presence: true, inclusion: { in: %w(on-site online hybrid) }
+  validates :auction_type, presence: true
   validates :terms_and_conditions, presence: true
   validates :terms_and_conditions, presence: true
 
   has_many :favourites, as: :favouritable
   has_many :products
+
+  enum auction_type: [ :'on-site', :'online', :hybrid ]
 
   aasm column: :state do
     state :new, initial: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,18 +5,12 @@ class Product < ApplicationRecord
   validates :bid_amount, presence: true
   validates :images, presence: true
 
-
   belongs_to :auction, optional: true
   belongs_to :article
   belongs_to :seller
   has_many_attached :images
   has_many :favourites, as: :favouritable
-end
 
-tax_included
-currency
-quantity
-unit_of_measure
-seller_id
-documents
-place_of_delivery
+  enum currency: [ :cop, :dollar ]
+  enum unit_of_measure: [ :u, :kg, :lt ]
+end
