d0cf6b679f5f1ec19bab5a6e40f2165f9a7d7397
blanquita <blanquita@MacBook-Air-de-blanquita.local>
2021-06-28 13:32:36 -0500

    Fix ssl with conect redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   uri = URI.parse(ENV["REDIS_URL"])
-  REDIS_INSTANCE = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  REDIS_INSTANCE = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, driver: :ruby)
 else
   REDIS_INSTANCE = Redis.new
 end
